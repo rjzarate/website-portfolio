2030cb7df167505b823fb6e952475b80934a5d66
Froilan Zarate Jr <zarate_rj@yahoo.com>
2025-07-20 20:03:08 -0700

    metadata update

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,12 @@ import { name } from "@/lib/constants";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: "Froilan Jr | Portfolio",
-    description: "Personal portfolio website of " + name + ", an aspiring developer",
+    title: "Froilan Jr — Portfolio",
+    description:
+        "Hello! This me! I'm Froilan Danganan Zarate Jr, and this is my portfolio website. I am an aspiring software engineer with a passion for building projects! Feel free to check my website to view what I've worked on and don't hesitate to contact me if you have a question or need assistance with your software 😊.",
+    openGraph: {
+        images: "/img/profile2.png",
+    },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
